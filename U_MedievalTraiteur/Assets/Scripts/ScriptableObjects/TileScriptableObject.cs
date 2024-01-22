266b52a9f49b2828ae0005d0cbca27d167226649
Sam De Boeck <sam@orointeractive.com>
2024-01-22 11:25:47 +0100

    - Tile SO changes

---
+++ U_MedievalTraiteur/Assets/Scripts/ScriptableObjects/TileScriptableObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "New Tile", menuName = "Medieval Traiteur/Tile", order = 0)]
+public class TileScriptableObject : ScriptableObject
+{
+    public string Name;
+    public string Description;
+
+    public int SoulValue;
+    [AssetSelector(Paths = "Assets/Prefabs/TilePrefabs")]
+
+    public Tile TilePrefab;
+    public Sprite UISprite;
+
+    [FormerlySerializedAs("_uiColor")] public Color UIColor;
+}
