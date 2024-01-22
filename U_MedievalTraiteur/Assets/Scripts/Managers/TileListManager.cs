7276d3ec8309c757eaa664d378052718c29ddfda
Sam De Boeck <sam@orointeractive.com>
2024-01-22 18:14:52 +0100

    - Updated Tile List Manager (WIP, still not working as intended)

--- U_MedievalTraiteur/Assets/Scripts/Managers/TileListManager.cs
+++ U_MedievalTraiteur/Assets/Scripts/Managers/TileListManager.cs
@@ -9,19 +9,36 @@ public class TileListManager : MonoBehaviour
     [SerializeField] private GameObject _tileUIPrefab;
     [SerializeField] private Transform _tileUIParent;
 
+    private int currentTileIndex = 0;
+
     private void Start()
     {
         ShuffleList(_tileList);
-        GenerateList();
+        PresentNextTile();
     }
 
-    private void GenerateList()
+    private void PresentNextTile()
     {
-        foreach (TileScriptableObject tileScriptableObject in _tileList)
+        if (currentTileIndex < _tileList.Count)
         {
+            TileScriptableObject tileScriptableObject = _tileList[currentTileIndex];
             GameObject newUITile = Instantiate(_tileUIPrefab, _tileUIParent);
             newUITile.GetComponent<TileUIObject>().SetupTileUIObject(tileScriptableObject);
+            currentTileIndex++;
         }
+        else
+        {
+            // Reshuffle and reset the index to 0
+            ShuffleList(_tileList);
+            currentTileIndex = 0;
+            PresentNextTile();
+        }
+    }
+
+    // Call this method when the player places a tile to present the next one
+    public void OnTilePlaced()
+    {
+        PresentNextTile();
     }
 
     // Fisher-Yates shuffle algorithm to shuffle the list
